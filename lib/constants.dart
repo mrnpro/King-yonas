f8130250faf1df49e4249390a03b44a7193fcd09
mrnpro <yegetanaty03@gmail.com>
2022-08-30 11:03:03 +0300

    initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kprimary = Color.fromARGB(255, 68, 0, 0);
+const kwhite = Color.fromARGB(255, 255, 255, 255);
+const kSecondaryWhite = Color.fromARGB(255, 185, 185, 185);
